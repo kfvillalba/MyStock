cc6b73765c834206ac0671145c3a9199825d1660
Jeison-R <jedaremo@gmail.com>
2024-04-16 01:26:49 -0500

    Consultando las salidas

--- src/pages/PageSalidas.jsx
+++ src/pages/PageSalidas.jsx
@@ -1,136 +1,120 @@
-import React, { useState, useEffect } from "react";
-import PanelDivisor from "../components/PanelDivisor";
-import DeleteIcon from "../assets/DeleteIcon";
-import EditIcon from "../assets/EditIcon";
-import Swal from "sweetalert2";
-import ModalRegisterFactura from "../components/ModalRegisterFactura";
-import ModalEditFactura from "../components/ModalEditFactura";
+import React, { useState, useEffect } from 'react'
+import PanelDivisor from '../components/PanelDivisor'
+import DeleteIcon from '../assets/DeleteIcon'
+import EditIcon from '../assets/EditIcon'
+import Swal from 'sweetalert2'
+import ModalRegisterFactura from '../components/ModalRegisterFactura'
+import ModalEditFactura from '../components/ModalEditFactura'
 
 const Page = () => {
-  const [facturas, setFacturas] = useState([
-    {
-      idFactura: "11",
-      fecha: "2024/232/232",
-      idCliente: "1003",
-      nombreCliente: "kevin Villlaba",
-      cantidadProductos: 213.213,
-      totalPagarConDescuento: 213.213,
-      totalPagarSinDescuento: 213.1234,
-      totalDescuento: 212.12312,
-      detalle: [
-        {
-          idCategoria: "",
-          nombreCategoria: "",
-          nombreProducto: "",
-          idProducto: "213123",
-          precio: 213.21,
-          cantidad: 213.12,
-          descuento: 21.312,
-          total: 2131.23,
-        },
-      ],
-    },
-  ]);
-  const [clientes, setClientes] = useState();
-  const [formRegister, setformRegister] = useState(false);
-  const [formEdit, setformEdit] = useState(false);
+  const [salidas, setSalidas] = useState([])
+
+  useEffect(() => {
+    fetch('https://localhost:7127/api/Salidas/ConsultarTodo')
+      .then((response) => response.json())
+      .then((data) => setSalidas(data))
+      .catch((error) => console.error('Error fetching data:', error))
+  }, [])
+  const [clientes, setClientes] = useState()
+  const [formRegister, setformRegister] = useState(false)
+  const [formEdit, setformEdit] = useState(false)
 
   return (
     <>
       <ModalRegisterFactura
         open={formRegister}
         onClose={() => {
-          setformRegister(false);
+          setformRegister(false)
         }}
         registrar={(dataForm) => {
-          console.log(dataForm);
-          setExistencia([...existencia, dataForm]);
+          console.log(dataForm)
+          setExistencia([...existencia, dataForm])
         }}
       />
       {
         <ModalEditFactura
           open={formEdit}
           onClose={() => {
-            setformEdit(false);
+            setformEdit(false)
           }}
           editar={(dataForm) => {}}
         />
       }
-      <div className="p-5  shadow-md rounded-sm shadow-black h-full">
+      <div className='p-5  shadow-md rounded-sm shadow-black h-full'>
         <h3>Lista Facturas</h3>
         <section>
           <button
             onClick={() => setformRegister(true)}
-            className="bnt__primary"
+            className='bnt__primary'
           >
             Nueva Factura
           </button>
         </section>
 
-        <section className="flex row-span-3 flex-wrap justify-between gap-4 my-5">
+        <section className='flex row-span-3 flex-wrap justify-between gap-4 my-5'>
           <div>
             <input
-              placeholder="Numero de factura"
-              type="number"
-              className="input__form"
+              placeholder='Numero de factura'
+              type='number'
+              className='input__form'
             />
           </div>
           <div>
-            <select type="text" className="select">
-              <option value="-1">Todos los Clientes</option>
+            <select type='text' className='select'>
+              <option value='-1'>Todos los Clientes</option>
               {clientes?.map((cliente) => {
                 return (
                   <option key={cliente.id} value={`${cliente.id}`}>
                     {cliente.nombre}
                   </option>
-                );
+                )
               })}
             </select>
           </div>
         </section>
 
-        <div className="h-3/4 overflow-y-auto snap-y shadow-sm shadow-black rounded-sm">
-          <table className="w-full table-auto ">
-            <thead className="[&>tr>th]:sticky [&>tr>th]:top-0 [&>tr>th]:py-2 [&>tr>th]:bg-purple-light [&>tr>th]:text-white">
+        <div className='h-3/4 overflow-y-auto snap-y shadow-sm shadow-black rounded-sm'>
+          <table className='w-full table-auto '>
+            <thead className='[&>tr>th]:sticky [&>tr>th]:top-0 [&>tr>th]:py-2 [&>tr>th]:bg-purple-light [&>tr>th]:text-white'>
               <tr>
-                <th className="text-start pl-3">Num Factura</th>
-                <th className="text-start pl-3">Fecha</th>
-                <th className="text-start pl-3">Cliente</th>
-                <th className="text-center">Total Pagado</th>
-                <th className="text-center">Editar</th>
-                <th className="text-center">Eliminar</th>
+                <th className='text-start pl-3'>Num Factura</th>
+                <th className='text-start pl-3'>Fecha</th>
+                <th className='text-start pl-3'>Cliente</th>
+                <th className='text-center'>Total Pagado</th>
+                <th className='text-center'>Editar</th>
+                <th className='text-center'>Eliminar</th>
               </tr>
             </thead>
             <tbody>
-              {facturas?.map((factura, index) => {
+              {salidas?.map((salida, index) => {
                 return (
-                  <tr className="even:bg-slate-100" key={index}>
-                    <td className="pl-3">{factura.idFactura}</td>
-                    <td>{factura.fecha}</td>
-                    <td>{factura.nombreCliente}</td>
-                    <td>{factura.totalPagarConDescuento}</td>
-                    <td className="text-center text-blue-800">
+                  <tr className='even:bg-slate-100' key={index}>
+                    <td className='pl-3'>{salida.id}</td>
+                    <td>{salida.fechaFactura}</td>
+                    <td>{salida.clienteNombre}</td>
+                    <td>{salida.totalPagarConDescuento}</td>
+                    <td className='text-center text-blue-800'>
                       <button onClick={(event) => setformEdit(true)}>
-                        <EditIcon clases={"size-7 cursor-pointer"} />
+                        <EditIcon clases={'size-7 cursor-pointer'} />
                       </button>
                     </td>
-                    <td className="text-center text-red-800">
-                      <button onClick={(event) => console.log("event")}>
-                        <DeleteIcon clases={"size-7 cursor-pointer"} />
+                    <td className='text-center text-red-800'>
+                      <button onClick={(event) => console.log('event')}>
+                        <DeleteIcon clases={'size-7 cursor-pointer'} />
                       </button>
                     </td>
                   </tr>
-                );
+                )
               })}
             </tbody>
           </table>
         </div>
       </div>
     </>
-  );
-};
+  )
+}
 const PageSalidas = () => {
-  return <PanelDivisor Page={<Page />} />;
-};
+  return <PanelDivisor Page={<Page />} />
+}
 
-export default PageSalidas;
+export default PageSalidas
