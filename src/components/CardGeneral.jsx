28230f169e3feaf6490b4fe1d01f8c4b6e53bbca
Jeison-R <jedaremo@gmail.com>
2024-04-29 19:24:01 -0500

    arreglo de yuma

--- src/components/CardGeneral.jsx
+++ src/components/CardGeneral.jsx
@@ -1,21 +1,20 @@
-import React from "react";
-import { Card } from "@tremor/react";
+import React from 'react'
+import { Card } from '@tremor/react'
 
 export function CardGeneral({ nombre, logo, cantidad, color }) {
   return (
     <Card
       className={`cardStyle [&>div>svg]:hover:-rotate-45 [&>div>svg]:transition [&>div>svg]:duration-300 ${color}`}
-      decoration="top"
     >
-      <div className="flex items-center justify-between">
+      <div className='flex items-center justify-between'>
         <div>
-          <p className="">{nombre}</p>
-          <p className="text-3xl text-tremor-content-strong dark:text-dark-tremor-content-strong font-semibold">
+          <p className=''>{nombre}</p>
+          <p className='text-3xl text-tremor-content-strong dark:text-dark-tremor-content-strong font-semibold'>
             {cantidad}
           </p>
         </div>
         {logo}
       </div>
     </Card>
-  );
+  )
 }
