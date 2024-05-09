f8e36f0d8b50ed60f047b01258cb4e7ec403333d
Jeison-R <jedaremo@gmail.com>
2024-05-08 20:08:19 -0500

    Posición del moda acomodada

--- src/components/modalVerUsuario.jsx
+++ src/components/modalVerUsuario.jsx
@@ -32,7 +32,10 @@ const ProfileModal = ({ open, onClose }) => {
   return (
     <>
       <div className='fixed w-full top-0 left-0 z-10 flex items-center justify-center'>
-        <div className='fixed inset-0 flex justify-end items-center mr-7'>
+        <div
+          className='fixed inset-0 flex justify-end items-center mr-7 '
+          style={{ height: '70vh' }}
+        >
           <div className='bg-slate-100 p-8 rounded-lg'>
             <section className='flex-wrap border-separate border-b-2 border-gray-300'>
               <div className='Profile flex flex-wrap items-center mb-3'>
@@ -41,7 +44,7 @@ const ProfileModal = ({ open, onClose }) => {
                   src={localStorage.getItem('photoURL')}
                   alt='avatar'
                 />
-                <div className='ml-3 text-sm'>
+                <div className='ml-3 text-sm text-center'>
                   <p className='text-bold'>
                     {localStorage.getItem('userName')?.length > 20
                       ? localStorage.getItem('userName').substring(0, 20) +
