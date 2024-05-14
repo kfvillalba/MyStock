8994fb1b71168ff69342a9c2f29294a025eb453f
Jeison-R <jedaremo@gmail.com>
2024-05-14 16:34:28 -0500

    Reportes funcionales (faltan los fitros)

--- src/pages/PageReportes.jsx
+++ src/pages/PageReportes.jsx
@@ -1,7 +1,18 @@
 import React, { useState, useEffect } from 'react'
 import PanelDivisor from '../components/PanelDivisor'
 import { useForm } from 'react-hook-form'
-import { generateVentasPDF } from '../components/TablasReportes'
+import {
+  generateVentasPDF,
+  generateSalidaClientesPDF,
+  generateComprasPDF,
+  generateEntradaProveedoresPDF,
+  generateProductosVendidosPDF,
+  generateSalidaClientesActualPDF,
+  generateProductosSalidaActualPDF,
+  generateProductosExistencia0PDF,
+  generateProductosBajaExistenciaPDF,
+} from '../components/TablasReportes'
+import Swal from 'sweetalert2'
 
 const Page = () => {
   const {
@@ -12,14 +23,60 @@ const Page = () => {
   } = useForm()
 
   const onSubmit = async (data) => {
-    if (tipoReporte === '1') {
-      try {
-        const pdfUrl = await generateVentasPDF(data)
-        setPdfData(pdfUrl)
-        setBotonTexto('Descargar PDF')
-      } catch (error) {
-        console.error(error)
+    console.log(data)
+    try {
+      let pdfUrl = ''
+      if (tipoReporte === '1') {
+        if (data.reporte === '1') {
+          pdfUrl = await generateVentasPDF(data)
+        } else if (data.reporte === '2') {
+          pdfUrl = await generateSalidaClientesPDF(data)
+        } else if (data.reporte === '3') {
+          pdfUrl = await generateComprasPDF(data)
+        } else if (data.reporte === '4') {
+          pdfUrl = await generateEntradaProveedoresPDF(data)
+        } else if (data.reporte === '5') {
+          pdfUrl = await generateProductosVendidosPDF(data)
+        }
       }
+      if (tipoReporte === '2') {
+        if (data.reporte === '1') {
+          pdfUrl = await generateVentasPDF()
+        } else if (data.reporte === '2') {
+          pdfUrl = await generateSalidaClientesActualPDF()
+        } else if (data.reporte === '3') {
+          // pdfUrl = await generateSalidaClientesActualPDF(data)
+        } else if (data.reporte === '4') {
+          // pdfUrl = await generateSalidaClientesActualPDF(data)
+        } else if (data.reporte === '5') {
+          pdfUrl = await generateProductosSalidaActualPDF()
+        }
+      }
+      if (tipoReporte === '3') {
+        if (data.reporte === '1') {
+          pdfUrl = await generateProductosExistencia0PDF()
+        } else if (data.reporte === '2') {
+          pdfUrl = await generateProductosBajaExistenciaPDF()
+        }
+      }
+      setPdfData(pdfUrl)
+      if (pdfUrl) {
+        Swal.fire({
+          icon: 'success',
+          title: 'Reporte generado correctamente',
+          showConfirmButton: false,
+          timer: 1500,
+        })
+      } else {
+        throw new Error('Error al generar el reporte')
+      }
+    } catch (error) {
+      console.error(error)
+      Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: 'Hubo un problema al generar el informe',
+      })
     }
   }
 
@@ -179,19 +236,6 @@ const Page = () => {
     setTipoReporte(event.target.value)
   }
 
-  const [botonTexto, setBotonTexto] = useState('Generar Reporte')
-
-  const handleButtonClick = () => {
-    if (pdfData) {
-      const link = document.createElement('a')
-      link.href = pdfData
-      link.download = 'reporte.pdf'
-      link.click()
-    } else {
-      document.getElementById('reporteForm').submit()
-    }
-  }
-
   return (
     <div
       className='gap-4 p-10 shadow-md  shadow-black '
@@ -227,14 +271,14 @@ const Page = () => {
                   className='input__form'
                   name='producto'
                   id='tipoReporte'
-                  {...register('producto')}
+                  {...register('reporte')}
                 >
                   <option value='-1'>Seleccione el tipo de reporte</option>
-                  <option value='-1'>Ventas</option>
-                  <option value='-1'>Salidas de clientes</option>
-                  <option value='-1'>Compras</option>
-                  <option value='-1'>Entrada de proveedores</option>
-                  <option value='-1'>Productos vendidos</option>
+                  <option value='1'>Ventas</option>
+                  <option value='2'>Salidas de clientes</option>
+                  <option value='3'>Compras</option>
+                  <option value='4'>Entrada de proveedores</option>
+                  <option value='5'>Productos vendidos</option>
                 </select>
                 <span className='message'>{errors?.producto?.message}</span>
               </div>
@@ -280,14 +324,14 @@ const Page = () => {
                   className='input__form'
                   name='producto'
                   id='tipoReporte'
-                  {...register('producto')}
+                  {...register('reporte')}
                 >
-                  <option value='-0'>Selecciones el tipo de reporte</option>
-                  <option value='-1'>Ventas</option>
-                  <option value='-2'>Cliente salidas</option>
-                  <option value='-3'>Compras</option>
-                  <option value='-4'>Entrada de proveedores</option>
-                  <option value='-5'>Salida de productos</option>
+                  <option value='-1'>Selecciones el tipo de reporte</option>
+                  <option value='1'>Ventas</option>
+                  <option value='2'>Cliente salidas</option>
+                  <option value='3'>Compras</option>
+                  <option value='4'>Entrada de proveedores</option>
+                  <option value='5'>Salida de productos</option>
                 </select>
                 <span className='message'>{errors?.producto?.message}</span>
               </div>
@@ -301,11 +345,11 @@ const Page = () => {
                   className='input__form'
                   name='producto'
                   id='tipoReporte'
-                  {...register('producto')}
+                  {...register('reporte')}
                 >
                   <option value='-1'>Seleccione el tipo de reporte</option>
-                  <option value='-1'>Productos sin existencia</option>
-                  <option value='-1'>Productos con baja existencia</option>
+                  <option value='1'>Productos sin existencia</option>
+                  <option value='2'>Productos con baja existencia</option>
                 </select>
                 <span className='message'>{errors?.producto?.message}</span>
               </div>
@@ -409,13 +453,18 @@ const Page = () => {
           </div>
         </div>
         <section className='self-end'>
-          <button
-            type='submit'
-            className='bnt__primary mt-5'
-            onClick={handleButtonClick}
-          >
-            {botonTexto}
+          <button type='submit' className='bnt__primary mt-5 '>
+            Generar Reporte
           </button>
+          {pdfData && (
+            <a
+              href={pdfData}
+              download='reporte.pdf'
+              className='mt-3 block text-center text-blue-500 font-medium'
+            >
+              Descargar PDF
+            </a>
+          )}
         </section>
       </form>
     </div>
