9d24cc4b921bb0b708a647ae2c59b076f62a504f
Jeison-R <jedaremo@gmail.com>
2024-05-21 22:00:24 -0500

    Una importación que marca que no se usa pero si sirve xd

--- src/components/Dashboard/LineChart.jsx
+++ src/components/Dashboard/LineChart.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Line } from 'react-chartjs-2'
+import Chart from 'chart.js/auto'
 
 const LineComprasVentas = ({ color }) => {
   const [ventasData, setVentasData] = useState([])
