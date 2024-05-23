efeee777777f24c0a31f6d159eff399d67b2deaf
Jeison-R <jedaremo@gmail.com>
2024-05-23 00:16:11 -0500

    Mimiendo yuma tranquilo.

--- src/components/Dashboard/CardGeneral.jsx
+++ src/components/Dashboard/CardGeneral.jsx
@@ -1,19 +1,19 @@
-import React from "react";
+import React from 'react'
 
 export function CardGeneral({ nombre, logo, cantidad, color }) {
   return (
     <div
-      className={`w-1/4 h-fit p-2 rounded-lg [&>div>svg]:hover:-rotate-45 [&>div>svg]:transition [&>div>svg]:duration-300 ${color}`}
+      className={`w-1/4 h-fit p-2 shadow-md shadow-gray-300 rounded-lg [&>div>svg]:hover:-rotate-45 [&>div>svg]:transition [&>div>svg]:duration-300 ${color}`}
     >
-      <div className="flex items-center justify-between">
+      <div className='flex items-center justify-between'>
         <div>
-          <p className="">{nombre}</p>
-          <p className="text-xl text-tremor-content-strong dark:text-dark-tremor-content-strong font-semibold">
+          <p className=''>{nombre}</p>
+          <p className='text-xl text-tremor-content-strong dark:text-dark-tremor-content-strong font-semibold'>
             {cantidad}
           </p>
         </div>
         {logo}
       </div>
     </div>
-  );
+  )
 }
--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,43 +1,43 @@
-import React, { useState, useEffect } from "react";
-import PanelDivisor from "../components/Pagina Principal/PanelDivisor";
-import ClienteIcon from "../assets/ClienteIcon";
-import ProductosIcon from "../assets/ProductosIcon";
-import { CardGeneral } from "../components/Dashboard/CardGeneral";
-import ProvedoresIcon from "../assets/ProveedoresIcon";
-import FacturaIcon from "../assets/FacturaIcon";
-import ExistenciaTotalIcon from "../assets/ExistenciaTotalIcon";
-import ExistenciaVendidaIcon from "../assets/ExistenciaVendidaIcon";
-import ExistenciaActualIcon from "../assets/ExistenciaActualIcon";
-import ImporteVendidoIcon from "../assets/ImporteVendido";
-import ImportePagadoIcon from "../assets/ImportePagado";
-import ImporteRestanteIcon from "../assets/ImporteRestante";
-import BeneficioBrutoIcon from "../assets/BeneficioBrutoIcon";
-import BeneficioTotalIcon from "../assets/BeneficioTotalIcon";
-import BarChartGanancias from "../components/Dashboard/BarChart";
-import LineComprasVentas from "../components/Dashboard/LineChart";
-import TopVendidos from "../components/Dashboard/DoughnutChart";
-import UtilidadProductos from "../components/Dashboard/utilidadProduct";
-import "../index.css";
+import React, { useState, useEffect } from 'react'
+import PanelDivisor from '../components/Pagina Principal/PanelDivisor'
+import ClienteIcon from '../assets/ClienteIcon'
+import ProductosIcon from '../assets/ProductosIcon'
+import { CardGeneral } from '../components/Dashboard/CardGeneral'
+import ProvedoresIcon from '../assets/ProveedoresIcon'
+import FacturaIcon from '../assets/FacturaIcon'
+import ExistenciaTotalIcon from '../assets/ExistenciaTotalIcon'
+import ExistenciaVendidaIcon from '../assets/ExistenciaVendidaIcon'
+import ExistenciaActualIcon from '../assets/ExistenciaActualIcon'
+import ImporteVendidoIcon from '../assets/ImporteVendido'
+import ImportePagadoIcon from '../assets/ImportePagado'
+import ImporteRestanteIcon from '../assets/ImporteRestante'
+import BeneficioBrutoIcon from '../assets/BeneficioBrutoIcon'
+import BeneficioTotalIcon from '../assets/BeneficioTotalIcon'
+import BarChartGanancias from '../components/Dashboard/BarChart'
+import LineComprasVentas from '../components/Dashboard/LineChart'
+import TopVendidos from '../components/Dashboard/DoughnutChart'
+import UtilidadProductos from '../components/Dashboard/utilidadProduct'
+import '../index.css'
 
 const fetchData = (url, setter) => {
   fetch(url)
     .then((response) => response.text())
-    .then((data) => setter(parseInt(data)));
-};
+    .then((data) => setter(parseInt(data)))
+}
 
 const Page = () => {
-  const [totalClientes, setTotalClientes] = useState(0);
-  const [totalProveedores, setTotalProveedores] = useState(0);
-  const [totalCategorias, setTotalCategorias] = useState(0);
-  const [totalProductos, setTotalProductos] = useState(0);
-  const [totalFacturas, setTotalFacturas] = useState(0);
-  const [existenciaTotal, setExistenciaTotal] = useState(0);
-  const [existenciaVendida, setExistenciaVendida] = useState(0);
-  const [existenciaActual, setExistenciaActual] = useState(0);
-  const [totalImporteVendido, setTotalImporteVendido] = useState(0);
-  const [totalImportePagado, setTotalImportePagado] = useState(0);
-  const [totalBeneficioBruto, setTotalbeneficioBruto] = useState(0);
-  const [totalBeneficioNeto, setTotalBeneficioNeto] = useState(0);
+  const [totalClientes, setTotalClientes] = useState(0)
+  const [totalProveedores, setTotalProveedores] = useState(0)
+  const [totalCategorias, setTotalCategorias] = useState(0)
+  const [totalProductos, setTotalProductos] = useState(0)
+  const [totalFacturas, setTotalFacturas] = useState(0)
+  const [existenciaTotal, setExistenciaTotal] = useState(0)
+  const [existenciaVendida, setExistenciaVendida] = useState(0)
+  const [existenciaActual, setExistenciaActual] = useState(0)
+  const [totalImporteVendido, setTotalImporteVendido] = useState(0)
+  const [totalImportePagado, setTotalImportePagado] = useState(0)
+  const [totalBeneficioBruto, setTotalbeneficioBruto] = useState(0)
+  const [totalBeneficioNeto, setTotalBeneficioNeto] = useState(0)
 
   const fetchItems = [
     // {
@@ -88,61 +88,61 @@ const Page = () => {
     //   url: 'https://localhost:7073/inventario-service/Dashboard/TarjetaBeneficioNeto',
     //   setter: setTotalBeneficioNeto,
     // },
-  ];
+  ]
 
   useEffect(() => {
-    fetchItems.forEach(({ url, setter }) => fetchData(url, setter));
-  }, []);
+    fetchItems.forEach(({ url, setter }) => fetchData(url, setter))
+  }, [])
 
   return (
-    <div className="shadow-md h-11-12 bg-[#f5f5fe]">
-      <section className="flex flex-col overflow-y-auto h-full gap-2 p-4 mb-9 ">
-        <div className="flex gap-3 justify-center">
+    <div className='shadow-md h-11-12 bg-[#f5f5fe]'>
+      <section className='flex flex-col overflow-y-auto h-full gap-2 p-4 mb-9 '>
+        <div className='flex gap-3 justify-center'>
           <CardGeneral
-            nombre={"Clientes"}
+            nombre={'Clientes'}
             cantidad={totalClientes}
-            logo={<ClienteIcon clases={"size-12"} />}
-            color={"bg-slate-300"}
+            logo={<ClienteIcon clases={'size-12'} />}
+            color={'bg-[#a0d88f]/30'}
           />
           <CardGeneral
-            nombre={"Clientes"}
+            nombre={'Clientes'}
             cantidad={totalClientes}
-            logo={<ClienteIcon clases={"size-12"} />}
-            color={"bg-slate-600"}
+            logo={<ClienteIcon clases={'size-12'} />}
+            color={'bg-[#81b872]/20'}
           />
           <CardGeneral
-            nombre={"Clientes"}
+            nombre={'Clientes'}
             cantidad={totalClientes}
-            logo={<ClienteIcon clases={"size-12"} />}
-            color={"bg-slate-100"}
+            logo={<ClienteIcon clases={'size-12'} />}
+            color={'bg-[#bef7ac]/30'}
           />
           <CardGeneral
-            nombre={"Clientes"}
+            nombre={'Clientes'}
             cantidad={totalClientes}
-            logo={<ClienteIcon clases={"size-12"} />}
-            color={"bg-slate-800"}
+            logo={<ClienteIcon clases={'size-12'} />}
+            color={'bg-[#a0d88f]/20'}
           />
           <CardGeneral
-            nombre={"Clientes"}
+            nombre={'Clientes'}
             cantidad={totalClientes}
-            logo={<ClienteIcon clases={"size-12"} />}
-            color={"bg-slate-200"}
+            logo={<ClienteIcon clases={'size-12'} />}
+            color={'bg-[#bef7ac]/30'}
           />
         </div>
-        <div className="flex justify-between gap-1">
+        <div className='flex justify-between gap-1'>
           <LineComprasVentas />
           <UtilidadProductos />
         </div>
-        <div className="flex justify-center gap-1">
+        <div className='flex justify-center gap-1'>
           <TopVendidos />
           <BarChartGanancias />
         </div>
       </section>
     </div>
-  );
-};
+  )
+}
 const DashboardPage = () => {
-  return <PanelDivisor Page={<Page />} />;
-};
+  return <PanelDivisor Page={<Page />} />
+}
 
-export default DashboardPage;
+export default DashboardPage
